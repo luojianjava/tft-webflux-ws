268441ccf2f05f27d3b6d64728846ba373eb6bac
luo_jian <luo_jian@dingtian.com.cn>
2020-11-30 14:28:47 +0800

    bug修复

--- src/main/java/com/tft/webflux/TftWebfluxWsApplication.java
+++ src/main/java/com/tft/webflux/TftWebfluxWsApplication.java
@@ -1,5 +1,6 @@
 package com.tft.webflux;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,9 +8,11 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
+@Slf4j
 public class TftWebfluxWsApplication {
 
     public static void main(String[] args) {
+        log.info( "线上bug修复版本" );
         SpringApplication.run(TftWebfluxWsApplication.class, args);
     }
 
